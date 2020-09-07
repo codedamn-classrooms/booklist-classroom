0c771cde5398ad658b29c7b7648b0dcbee0d9967
Mehul Mohan <mehulmpt@gmail.com>
2020-09-08 01:11:43 +0530

    Lab 10 sol

--- script.js
+++ script.js
@@ -9,14 +9,31 @@ class Book {
 
 class Store {
 	static addBook(book) {
-
-    }
+		const books = Store.getBooks()
+		books.push(book)
+		localStorage.setItem('books', JSON.stringify(books))
+	}
 	static removeBook(isbn) {
+		const books = Store.getBooks()
 
-    }
+		books.forEach((book, index) => {
+			if (book.isbn === isbn) {
+				books.splice(index, 1)
+			}
+		})
+
+		localStorage.setItem('books', JSON.stringify(books))
+	}
 	static getBooks() {
-        
-    }
+		let books
+		if (!localStorage.getItem('books')) {
+			books = []
+		} else {
+			books = JSON.parse(localStorage.getItem('books'))
+		}
+
+		return books
+	}
 }
 
 // UI Class: Handle UI Tasks
