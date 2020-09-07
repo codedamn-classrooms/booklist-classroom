8926a025ecb3b59ecdfeb04c6ca36546995f378c
Mehul Mohan <mehulmpt@gmail.com>
2020-09-07 20:17:42 +0530

    Lab 3 solution

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ const defaultBooks = [
 ]
 
 // UI Class: Handle UI Tasks
+class UI {
+	static displayBooks() {
+		defaultBooks.forEach((book) => UI.addBookToList(book))
+	}
+	static addBookToList(book) {
+		const list = document.getElementById('book-list')
+
+		const row = document.createElement('tr')
+
+		row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `
+
+		list.appendChild(row)
+	}
+}
+
+UI.displayBooks()
