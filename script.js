58214f2b97c07d356a9113f5a820f5d33762b93c
Mehul Mohan <mehulmpt@gmail.com>
2020-09-07 20:06:43 +0530

    Default files

---
+++ script.js
@@ -0,0 +1,23 @@
+// Book Class: Represents a Book
+class Book {
+	constructor(title, author, isbn) {
+		this.title = title
+		this.author = author
+		this.isbn = isbn
+	}
+}
+
+const defaultBooks = [
+	{
+		title: 'Book 1',
+		author: 'Brad Traversy',
+		isbn: '12345'
+	},
+	{
+		title: 'Book 2',
+		author: 'Mehul Mohan',
+		isbn: '6789'
+	}
+]
+
+// UI Class: Handle UI Tasks
